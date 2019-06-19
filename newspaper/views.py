3f629ad7afbb98b6b2be1cc7ee7a05166ba879a1
Yamilet <yoliva@emergya.com>
2019-06-19 21:57:17 +0200

    Create models

---
+++ newspaper/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
+
+from .models import BaseItems, New, Event
