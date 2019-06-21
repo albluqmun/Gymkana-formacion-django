cb68bf2edb346dd6faeee9074e062bb0391ce4ea
Yamilet <yoliva@emergya.com>
2019-06-21 10:27:04 +0200

    Creating Panel Admin

--- newspaper/urls.py
+++ newspaper/urls.py
@@ -2,4 +2,10 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = []
+app_name = 'newspaper'
+
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.NewView.as_view(), name='new'),
+    path('<int:pk>/event/', views.EventView.as_view(), name='event'),
+]
