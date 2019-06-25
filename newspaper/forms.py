1bad2faf2386f9282c7624e3fcb756a8e49680a6
Yamilet <yoliva@emergya.com>
2019-06-25 12:47:03 +0200

    Create News

---
+++ newspaper/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from .models import New
+from django.core.validators import FileExtensionValidator
+from django.core.exceptions import ValidationError
+
+
+class NewsForm(forms.ModelForm):
+    image = forms.ImageField(validators=[FileExtensionValidator(allowed_extensions=['jpg', 'png'])], required=False)
+
+    class Meta:
+        model = New
+        fields = ('id', 'title', 'image', 'subtitle', 'body',)
+    
+    def clean_image(self):
+        image = self.cleaned_data.get('image')
+        max_size=10*1024*1024
+
+        if image.size > max_size:
+            raise ValidationError("The maximum file size that can be uploaded is 10MB")
+
--- newspaper/urls.py
+++ newspaper/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-
 from . import views
 
 
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('v1/news/create', views.create_news, name='create_news'),
 ]
