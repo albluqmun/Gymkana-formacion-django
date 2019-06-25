eb534ae18ab4d8a41116dd1ff7c371854245be77
Yamilet <yoliva@emergya.com>
2019-06-26 01:35:03 +0200

    News update

--- newspaper/urls.py
+++ newspaper/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('v1/news/create', views.create_news, name='create_news'),
     path('v1/news/view', views.news_view, name='news_view'),
     path('v1/news/view/<int:news_id>', views.news_view_detail, name='news_view_detail'),
+    path('v1/news/view/update/<int:news_id>', views.news_update, name='news_update'),
 ]
