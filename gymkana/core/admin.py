0b5493e85dea691a3a955e76e1a27e3f839cf654
AlexBCa <alexbeca92@gmail.com>
2022-04-18 17:21:01 +0200

    create admin

--- gymkana/core/admin.py
+++ gymkana/core/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import New, Event
 
-# Register your models here.
+@admin.register(New)
+class NewAdmin(admin.ModelAdmin):
+    list_display = ('title', 'subtitle', 'publish_date')
+    list_filter = ('publish_date',)
+    search_fields = ('title', 'subtitle')
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'subtitle', 'start_date', 'end_date')
+    list_filter = ('start_date', 'end_date')
+    search_fields = ('title', 'subtitle')
