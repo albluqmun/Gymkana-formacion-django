cc68c282367c490de5ee52c0eac865fa3764df6b
AlexBCa <alexbeca92@gmail.com>
2022-04-18 17:19:54 +0200

    create models

--- gymkana/core/models.py
+++ gymkana/core/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class BaseItems(models.Model):
+    title = models.CharField(max_length=100)
+    subtitle = models.CharField(max_length=100)
+    body = models.TextField()
+
+    def __str__(self) -> str:
+        return self.title
+  
+    class Meta:
+        abstract = True
+
+class New(BaseItems):
+    publish_date = models.DateField(auto_now_add=True)
+    image = models.ImageField(upload_to='', default='default.jpg', blank=True)
+
+
+class Event(BaseItems):
+    start_date = models.DateField()
+    end_date = models.DateField()
