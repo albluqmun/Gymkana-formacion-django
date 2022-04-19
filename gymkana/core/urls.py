1c2db7ef2422bdd4807caf003c074460446b092e
AlexBCa <alexbeca92@gmail.com>
2022-04-19 10:29:06 +0200

    add /v

--- gymkana/core/urls.py
+++ gymkana/core/urls.py
@@ -4,7 +4,7 @@ from .views import index, detail_news, list_news
 
 urlpatterns = [
     path('', index, name='index'),
-    path('news/', list_news, name='list_news'),
-    path('news/<int:pk>/', detail_news, name='detail_news'),
+    path('v/news', list_news, name='list_news'),
+    path('v/news/<int:pk>/', detail_news, name='detail_news'),
     
 ]
