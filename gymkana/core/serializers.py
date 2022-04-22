e613ab8f877b3708c0a4802c9cfcd5ce107a8f6d
AlexBCa <alexbeca92@gmail.com>
2022-04-22 14:49:16 +0200

    add validator dates

--- gymkana/core/serializers.py
+++ gymkana/core/serializers.py
@@ -16,3 +16,11 @@ class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
         fields = ('id', 'title', 'subtitle', 'body', 'start_date', 'end_date')
+
+    # validate start_date and end_date
+    def validate(self, data):
+        start_date = data['start_date']
+        end_date = data['end_date']
+        if start_date > end_date:
+            raise serializers.ValidationError("La fecha de inicio debe ser menor a la fecha de fin")
+        return data
