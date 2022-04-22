31875e704b3d31e48f68f8ec8201909096232229
AlexBCa <alexbeca92@gmail.com>
2022-04-22 14:38:01 +0200

    add validate size image 10mb

--- gymkana/core/serializers.py
+++ gymkana/core/serializers.py
@@ -6,6 +6,12 @@ class NewSerializer(serializers.ModelSerializer):
         model = New
         fields = ('id', 'title', 'subtitle', 'body', 'image')
 
+    # validate image size
+    def validate_image(self, value):
+        if value.size > (1024 * 1024 * 10):
+            raise serializers.ValidationError("El tamaño de la imagen no debe superar los 10MB")
+        return value
+
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
